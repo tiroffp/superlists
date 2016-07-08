951d0fe086197235e1add5216b6cfe98161f3dde
tiroffp <tiroff.p@husky.neu.edu>
2016-07-08 12:07:42 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -16,20 +17,32 @@ class NewVisitorTest(unittest.TestCase):
         # and sees an ad for a to-do list site. She goes to check out the
         #homepage
         self.browser.get('http://localhost:8000')
-        #She notices that the title mentions to-do lists, so she
+
+        # She notices that the title mentions to-do lists, so she
         # knows shes on the right site
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("To-Do", header_text)
+
         #She is invited to make a list right away
+        inputbox = self.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item')
 
         # She types "Buy more artisanal cheeses"
+        inputbox.send_keys('Buy more artisanal cheeses')
 
         # When she hits enter, the page updates, and the page now
         # lists "1: Buy more artisanal cheeses" as an item on a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy more artisanal cheeses' for row in rows))
 
         # There is still a textbox inviting here to enter another item
         # she enters "Host a fancy party with fancy cheeses"
-
+        self.fail('Finish the test!')
         # the page updates again, showing both her inputs
 
         # Mary begins to wonder if the site will remember her list, but the
