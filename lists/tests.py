9e173caa9f59c063b47e5781a41e3e4af8b05901
tiroffp <tiroff.p@husky.neu.edu>
2016-07-11 16:34:55 -0400

    broke lists out into URLS unique from the home URL

--- lists/tests.py
+++ lists/tests.py
@@ -18,29 +18,6 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_save_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = "A new list item"
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = "A new list item"
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/only/')
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
 
@@ -62,7 +39,7 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
 
 
-class ListViewtest(TestCase):
+class ListViewTest(TestCase):
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemy 1')
         Item.objects.create(text='itemy 2')
@@ -75,3 +52,20 @@ class ListViewtest(TestCase):
     def test_uses_list_template(self):
         response = self.client.get('/lists/only/')
         self.assertTemplateUsed(response, 'list.html')
+
+
+class NewListTest(TestCase):
+    def test_saving_a_POST_request(self):
+        self.client.post('/lists/new',
+                         data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new',
+                                    data={'item_text': 'A new list item'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/lists/only/')
