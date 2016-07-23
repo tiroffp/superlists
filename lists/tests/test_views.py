6a380fdca86e48bf7b87868f931c87e2b7779a5b
tiroffp@gmail.com <tiroff.p@husky.neu.edu>
2016-07-23 15:51:42 -0400

    Adjust new list view to do model validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from lists.views import home_page
+from django.utils.html import escape
 from django.template.loader import render_to_string
 from lists.models import Item, List
 
@@ -50,7 +51,6 @@ class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
         self.client.post('/lists/new',
                          data={'item_text': 'A new list item'})
-
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
@@ -60,6 +60,18 @@ class NewListTest(TestCase):
                                     data={'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self. assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('lists/new', data={'item_text': ''})
+        self.assertEquals(List.objects.count(), 0)
+        self.assertEquals(Item.objects.count(), 0)
 
 
 class NewItemTest(TestCase):
