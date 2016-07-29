a39491d6af85c8e79eb80af054b3a0673b26dcb9
tiroffp <tiroff.p@husky.neu.edu>
2016-07-29 17:24:41 -0400

    added ft for removing error message and prelim javascrpit code added

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,6 +4,9 @@ from functional_tests.base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Mary goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
@@ -11,7 +14,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('\n')
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
         # She tries again with some text for the item, which now works
         self.get_item_input_box().send_keys('Buy milk\n')
@@ -20,7 +23,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('\n')
         # She receives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
         # And she can correct it by filling some text in
         self.get_item_input_box().send_keys('Make tea\n')
@@ -35,5 +38,19 @@ class ItemValidationTest(FunctionalTest):
 
         # She accidentally tries to enter a duplicate item
         self.get_item_input_box().send_keys('Buy wellies\n')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Mary starts a new list in a way that causes a validation error
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is pleased to see the error message disappears
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
